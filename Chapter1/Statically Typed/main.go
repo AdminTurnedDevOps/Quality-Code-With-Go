f38d86a984b448d62c6822c9ec90f30e1014e98d
AdminTurnedDevOps <mlevan1992@gmail.com>
2020-09-20 14:19:35 -0400

    Statically typed and variables

---
+++ Chapter1/Statically Typed/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test() {
+	var carType string = "Ford"
+	var owned int = 1
+	
+	fmt.Println(carType + owned)
+}
---
+++ Chapter1/Variables/Different-Ways-To-Define-Variables/main.go
@@ -0,0 +1,14 @@
+// Declaring a single variable without a default value
+
+var carType string
+
+// Declaring a single variable with a default value
+
+var carType string = "Ford"
+
+// Multiple variable declaration - Must be the same type
+
+var carType, carYear string = "Ford", "2020"
+
+// Shorthand variable declaration
+carType := "Ford"
