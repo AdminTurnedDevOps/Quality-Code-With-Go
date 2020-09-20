892b9b04607bcbf9e95f2c9b589535eb28945001
AdminTurnedDevOps <mlevan1992@gmail.com>
2020-09-20 13:27:19 -0400

    Importing packages

---
+++ Chapter1/Importing-Packages/main.go
@@ -0,0 +1,17 @@
+// Method 1 - All packages in one import statement
+
+import (
+	"fmt"
+	"os"
+)
+
+// Method 2 - Line by line
+import "fmt"
+import "os"
+
+// Method 3 - Automation
+import (
+	"fmt"
+)
+
+fmt.Println("Hello, readers!")
