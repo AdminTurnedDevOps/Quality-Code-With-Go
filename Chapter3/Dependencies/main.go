b3b3f831cba9b0d5191bc3492fa6f326c4e2cb5f
Michael Levan <mlevan1992@gmail.com>
2020-10-03 08:42:37 -0400

    Chapter 3 dependency changes

---
+++ Chapter3/Dependencies/main.go
@@ -0,0 +1,5 @@
+// Pulling from master
+go get github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute
+
+// Pulling from a different branch, for example, the `dev` branch
+go get github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute@dev
