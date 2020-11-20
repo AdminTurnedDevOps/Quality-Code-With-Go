9b3cf9b6f3de151af2cc303940225fe8b515740d
Varun Gupta <varungupta2015135@gmail.com>
2020-11-20 16:34:44 +0530

    PEP 8 -- Style Guide for Python Code
    https://www.reddit.com/r/golang/comments/jx4zyl/quality_code_with_go_self_published_book_finished/gcvqwy0?utm_source=share&utm_medium=web2x&context=3

--- Chapter1/Object-Oriented/class.py
+++ Chapter1/Object-Oriented/class.py
@@ -1,9 +1,9 @@
 class Cars:
-    def __init__(self, Make, Model, Year):
-        self.Make = Make
-        self.Model = Model
-        self.Year = Year
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
 myCar = Cars("Ford", "F150", "2020")
 
-print(myCar.Make)
+print(myCar.make)
